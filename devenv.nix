182b397d796434eedf9400f338be49b86f05c74b
y-cg <159802291+y-cg@users.noreply.github.com>
2025-11-13 15:49:23 +0800

    hello world

---
+++ devenv.nix
@@ -0,0 +1,48 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable {
+    system = pkgs.stdenv.system;
+    config = {
+      allowUnfree = true;
+    };
+  };
+in
+{
+  imports = [
+    inputs.githooks.modules.default
+  ];
+
+  custom.git-hooks = {
+    jupyter-notebook.enable = true;
+  };
+
+  # https://devenv.sh/packages/
+  packages = with pkgs-unstable; [ claude-code ];
+
+  # https://devenv.sh/languages/
+  languages = {
+    nix.enable = true;
+    python = {
+      enable = true;
+      venv.enable = true;
+      uv = {
+        enable = true;
+        sync.enable = true;
+      };
+    };
+  };
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    ruff.enable = true;
+    ruff-format.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
