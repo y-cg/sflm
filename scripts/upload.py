a35a56aee4c5f5a8eba838b1ae4e50425239be8c
y-cg <159802291+y-cg@users.noreply.github.com>
2025-11-14 13:26:24 +0800

    feat: implement conversion script

--- scripts/upload.py
+++ scripts/upload.py
@@ -4,6 +4,11 @@ from pathlib import Path
 import s3fs
 from datetime import datetime
 
+import torch
+from dotenv import load_dotenv
+
+from sflm.model.sflm import SFLM
+
 
 def timestamp() -> str:
     return datetime.now().strftime("%Y%m%d%H%M")
@@ -13,11 +18,26 @@ def convert_model_ckpt(base_dir: Path):
     # find all ckpt files in base_dir
     ckpts = list(base_dir.rglob("*.ckpt"))
     for ckpt in ckpts:
-        pass
-    raise NotImplementedError
+        model = SFLM.load_from_checkpoint(ckpt)
+        torch.save(
+            dict(
+                model_config={
+                    "vocab_size": model.vocab_size,
+                    "emb_dim": model.emb_dim,
+                    "block_size": model.block_size,
+                },
+                state_dict=model.state_dict(),
+            ),
+            f"{ckpt.parent}/model.sav",
+        )
+        print(f"Converted {ckpt} to {ckpt.parent}/model.sav")
 
 
 if __name__ == "__main__":
+    # dotenv injection
+    load_dotenv()
+
+    convert_model_ckpt(Path("lightning_logs/"))
     # upload results to s3
     s3 = s3fs.S3FileSystem(
         key=os.getenv("S3_ACCESS_KEY"),
@@ -27,6 +47,6 @@ if __name__ == "__main__":
 
     s3.upload(
         "lightning_logs/",
-        f"{os.getenv('S3_BUCKET')}/lightning_logs/{timestamp()}",
+        f"{os.getenv('S3_BUCKET_NAME')}/lightning_logs-{timestamp()}",
         recursive=True,
     )
